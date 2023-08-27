48b5528b11a9c94f8318d1bf0ebba080148f85ed
Casey Cheng <61931277+YiGuan-z@users.noreply.github.com>
2023-08-27 20:09:23 +0800

    修复在addView中，输入内容的时候会将按钮顶开的bug。 Fix the bug that the button will be opened when entering content in addView.

--- TODO/Views/AddView.swift
+++ TODO/Views/AddView.swift
@@ -13,53 +13,45 @@ struct AddView: View {
     
     @EnvironmentObject var listViewModel:ListViewModel
     
-    @State var textFieldText:String = ""
+    @State var title:String = ""
     @State var alertTitle:String = ""
     @State var showAlert:Bool = false
     @State var conetnt:String = ""
+    
     var body: some View {
-//        ScrollView{
             VStack{
-                TextField("事件名称", text: $textFieldText)
-                    .padding(.horizontal)
-                    .frame(height: 55)
-                    .background(Color(UIColor.secondarySystemBackground))
-                    .cornerRadius(10)
-                
-                TextEditor(text: $conetnt)
-                    .frame(height: AppState.screenHight*0.6)
-                    .background(Color.red)
-
-                HStack{
-                    NavigationLink {
-                        ShowMarkDownView(content: $conetnt)
-                    } label: {
-                        Text("查看效果")
-                            .withTextToButtonLabel(fontColor: .white, font: .headline, backgroundColor: .green)
+                ScrollView{
+                    VStack{
+                        TextField("事件名称", text: $title)
+                            .padding(.horizontal)
+                            .frame(height: 55)
+                            .background(Color(UIColor.secondarySystemBackground))
+                            .cornerRadius(10)
+                        
+                        TextEditor(text: $conetnt)
+                            .frame(height: AppState.screenHight*0.6)
                     }
-
-                    Button(action: saveButtonPressed, label: {
-                        Text("save".uppercased())
-                            .withTextToButtonLabel(fontColor: .white, font: .headline, backgroundColor: .accentColor)
-                    })
+                    .padding(14)
+                    .navigationTitle("添加活动")
+                    .alert(isPresented: $showAlert, content: getAlert)
                 }
+                Spacer()
                 
+                bottomfloatingButton
+                    .padding(14)
             }
-            .padding(14)
-//        }
-        .navigationTitle("添加活动")
-        .alert(isPresented: $showAlert, content: getAlert)
-    }
+        }
+
     
     func saveButtonPressed(){
         if textIsApproriate(){
-            self.listViewModel.addItem(title: self.textFieldText,content: self.conetnt)
+            self.listViewModel.addItem(title: self.title,content: self.conetnt)
             presentationMode.wrappedValue.dismiss()
         }
     }
     
     func textIsApproriate()->Bool{
-        if textFieldText.isEmpty{
+        if title.isEmpty{
             alertTitle = "事件名称不能为空"
             showAlert.toggle()
             return false
@@ -69,6 +61,24 @@ struct AddView: View {
     
     func getAlert()->Alert{
         return Alert(title: Text(self.alertTitle))
+    }
+}
+
+extension AddView{
+    private var bottomfloatingButton:some View{
+        HStack{
+            NavigationLink {
+                ShowMarkDownView(content: $conetnt)
+            } label: {
+                Text("查看效果")
+                    .withTextToButtonLabel(fontColor: .white, font: .headline, backgroundColor: .green)
+            }
+            
+            Button(action: saveButtonPressed, label: {
+                Text("save".uppercased())
+                    .withTextToButtonLabel(fontColor: .white, font: .headline, backgroundColor: .accentColor)
+            })
+        }
     }
 }
 
